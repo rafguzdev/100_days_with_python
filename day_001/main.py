639b7982fb60bdca387e345329e82b69c8f3e677
rafguzdev <rafguzdev@gmail.com>
2021-06-12 11:48:30 +0200

    day 001 finished

--- day_001/main.py
+++ day_001/main.py
@@ -1,1 +1,19 @@
-print('hello')
+# Task 1 - len() function
+word = input('Type some word: ')
+print(f'This string have {len(word)} characters')
+
+# Task 2 - switch values
+a = input('a: ')
+b = input('b: ')
+
+temp = a
+a = b
+b = temp
+print(f'a: {a}')
+print(f'b: {b}')
+
+# Task 3 - Final
+print('Welcome to the Band Name Generator.')
+city = input('What is name of the city you grew up in? ')
+pet = input('What is your pet name? ')
+print(f'Your band name could be {city} {pet}')

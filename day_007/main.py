354c983ba72d92b157eeb3addbf4381891410efd
rafguzdev <rafguzdev@gmail.com>
2021-06-18 21:39:21 +0200

    hangman game

---
+++ day_007/main.py
@@ -0,0 +1,20 @@
+# Hangman
+import random
+word_list = ['ardvark', 'baboon', 'camel']
+rand_word = random.choice(word_list)
+hide_word = []
+for letter in rand_word:
+    hide_word.append('_')
+
+while True:
+    user_letter = input('type a leter: ')
+    for idx, letter in enumerate(rand_word):
+        if letter == user_letter:
+            hide_word[idx] = letter
+    print(hide_word)
+    if '_' not in hide_word:
+        print('you win')
+        break
+
+
+

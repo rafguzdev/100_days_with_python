731566c3b0b8c73b220141a7f497707c90af20c6
rafguzdev <rafguzdev@gmail.com>
2021-07-19 20:58:35 +0200

    Day 38 - Intermediate+ Workout Tracking

---
+++ day_038/main.py
@@ -0,0 +1,48 @@
+import requests
+from datetime import datetime
+
+GENDER = 'male'
+WEIGHT_KG = 95
+HEIGHT_CM = 198
+AGE = 30
+
+APP_ID = '9938ffb322'
+API_KEY = '006d9d617a5d05d70a162e92b322f652ee'
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+exercise_text = input("Tell me which exercises you did: ")
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
+
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response = requests.post(exercise_endpoint, json=parameters, headers=headers)
+result = response.json()
+print(result)
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    print(exercise)
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+print(sheet_inputs)
